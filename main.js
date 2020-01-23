633d1acb73b2d7893fc05e2167debd84c8bb6dc8
acocalypso <st.luzifer6663@hotmail.com>
2020-01-23 15:51:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ copyright by acocalypso 2020
  */
 
 const Discord = require('discord.io');
